ddf1716942ca9d0123b98b17fcac36b5cea13e15
unknown <Chetan-KK>
2023-03-30 19:44:03 +0530

    layout shifting text fixed for mobile

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Chetan-KK/"
+  base: "/Chetan-KK/",
   // server: {
   //   origin: 'http://localhost:3000',
   //   host: '0.0.0.0',
