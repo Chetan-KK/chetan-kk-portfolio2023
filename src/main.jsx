a468068d9fa2e6fe49ca62fd18c3a716143af1e1
unknown <Chetan-KK>
2023-04-09 00:25:55 +0530

    refresh bug fixed

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Chetan-KK">
       <App />
     </BrowserRouter>
   </React.StrictMode>
